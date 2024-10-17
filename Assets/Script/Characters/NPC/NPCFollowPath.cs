ce77d4427d10460fde709080ccb0b0ea3fa7798e
Rizky Dwi Purnama <rizkypurnama2002@gmail.com>
2024-10-17 10:26:47 +0700

    ganti metode patroli ke script based

---
+++ Assets/Script/Characters/NPC/NPCFollowPath.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCFollowPath : MonoBehaviour
+{
+    [SerializeField] Transform[] path;
+    [SerializeField] Animator animator;
+
+    int pathIndex;
+
+    void OnAnimatorMove()
+    {
+        transform.position = animator.rootPosition;
+        transform.LookAt(path[pathIndex].transform);
+        transform.eulerAngles.Set(0, transform.eulerAngles.y, 0);
+
+        if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit, 100f))
+        {
+            transform.position = new Vector3(transform.position.x, hit.point.y, transform.position.z);
+        }
+
+        if (Vector3.Distance(transform.position, path[pathIndex].transform.position) < 0.25f)
+        {
+            pathIndex = (pathIndex + 1) % path.Length;
+        }
+    }
+}
